d70e27ae4394bc1377a0c028f2a1b02b33e9419b
Patryk Dynda <PDyndal@gmail.com>
2020-12-01 19:22:45 +0100

    Added Laravel, In- and OutgoingTransfer Models, Factories, Migrations, Seeders and Controllers

---
+++ api/database/factories/OutgoingTransferFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\OutgoingTransfer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OutgoingTransferFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = OutgoingTransfer::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
