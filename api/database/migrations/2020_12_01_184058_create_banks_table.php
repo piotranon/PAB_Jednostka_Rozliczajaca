a6b26460d5d8a7ed44cbd7433dfe4e2da1433d2b
Patryk Dynda <PDyndal@gmail.com>
2020-12-01 19:42:04 +0100

    Added Bank Model package.

--- api/database/migrations/2020_12_01_184058_create_banks_table.php
+++ api/database/migrations/2020_12_01_184058_create_banks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOutgoingTransfersTable extends Migration
+class CreateBanksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,8 @@ class CreateOutgoingTransfersTable extends Migration
      */
     public function up()
     {
-        Schema::create('outgoing_transfers', function (Blueprint $table) {
+        Schema::create('banks', function (Blueprint $table) {
             $table->id();
-            $table->
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ class CreateOutgoingTransfersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('outgoing_transfers');
+        Schema::dropIfExists('banks');
     }
 }
